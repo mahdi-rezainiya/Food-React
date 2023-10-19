b56308a5172a51a2f7c156455bb52e4371857b57
mahdi rezainiya <mahdi.rezainiya@gmail.com>
2023-10-19 13:40:46 +0330

    New Changes In Project

--- src/pages/Recipe/Recipe.js
+++ src/pages/Recipe/Recipe.js
@@ -3,7 +3,8 @@ import './Recipe.css';
 // import { useFetch } from '../../hooks/useFetch';
 import { useTheme } from '../../hooks/useTheme';
 import { useEffect, useState } from 'react';
-import { doc, getDoc } from 'firebase/firestore';
+// import { doc, getDoc, onSnapshot, updateDoc } from 'firebase/firestore';
+import { doc, onSnapshot, updateDoc } from 'firebase/firestore';
 import { db } from '../../firebase/config.js';
 
 
@@ -17,23 +18,55 @@ export default function Recipe() {
     const [isLoading , setIsLoading] = useState(false);
     const [error , setError] = useState(false);
 
+    // useEffect(() => {
+    //     setIsLoading(true)
+
+    //     const ref = doc(db , 'recipes' , id)
+    //     getDoc(ref)
+    //         .then((doc) => {
+    //             if(doc.empty){
+    //                 setError('NO Recipe To Load . . .')
+    //                 setIsLoading(false)
+    //             }
+    //             else{
+    //                 setIsLoading(false)
+    //                 setRecipe(doc.data())
+    //             }
+    //         })
+    // } , [])
+
+
     useEffect(() => {
         setIsLoading(true)
 
         const ref = doc(db , 'recipes' , id)
-        getDoc(ref)
-            .then((doc) => {
-                if(doc.empty){
-                    setError('NO Recipe To Load . . .')
-                    setIsLoading(false)
-                }
-                else{
-                    setIsLoading(false)
-                    setRecipe(doc.data())
-                }
-            })
 
-    } , [])
+        const unsub = onSnapshot(ref , (snapshot) => {
+            if(snapshot.empty){
+                setError('NO Recipe To Load . . .')
+                setIsLoading(false)
+            }
+            else{
+                setIsLoading(false)
+                setRecipe(snapshot.data())
+            }
+        })
+
+        return () => unsub()
+
+    } , [id])
+
+    const handleClick = async () => {
+        try{
+            const ref = doc(db , 'recipes' , id)
+            await updateDoc(ref , {
+                title : 'something new'
+            })
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
 
     return (
         <div className={`recipe ${mode}`}>
@@ -47,6 +80,7 @@ export default function Recipe() {
                         {recipe.ingredients.map(ing => <li key={ing}>{ing}</li>)}
                     </ul>
                     <p className='method'>{recipe.method}</p>
+                    <button onClick={handleClick}>Update the Title</button>
                 </>
             )}
         </div>
